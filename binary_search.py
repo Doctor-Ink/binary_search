bcc8a218f73c1c2bd381ee950b44b85ed012d99a
Doctor-Ink <bezusov.88@mail.ru>
2022-08-08 23:05:20 +0300

    file: add commets

--- binary_search.py
+++ binary_search.py
@@ -1,18 +1,18 @@
 # Функция бинарного поиска из книги "Грокаем алгоритмы" Адитья Бхаргава
 
-def binary_search(list, item):
-    low = 0
+def binary_search(list, item):    # В переменных low и high хранятся границы
+    low = 0                       # той части списка, в которой выполняется поиск
     high = len(list) - 1
-    while low <= high:
-        mid = (low + high) // 2
+    while low <= high:              # <--- пока эта часть не сократится до одного элемента...
+        mid = (low + high) // 2     # <--- проверяем средний элемент
         guess = list[mid]
-        if guess == item:
+        if guess == item:           # <--- значение найдено
             return mid
-        if guess > item:
+        if guess > item:            # <--- Много
             high = mid - 1
-        else:
+        else:                       # <--- Мало
             low = mid + 1
-    return None
+    return None                     # <--- Значение не существует
 
 my_list = [1, 3, 5, 7, 9]
 print(binary_search(list=my_list, item=3))
